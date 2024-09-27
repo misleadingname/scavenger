71bed6c6844e70228d1b51f22109dc4f363349c8
MisleadingName <49401941+misleadingname@users.noreply.github.com>
2024-09-27 08:31:55 +0200

    Remove test code

--- App/Header.php
+++ App/Header.php
@@ -8,9 +8,3 @@ use Scavenger\Internals\Bases\BaseDatabase;
 use Scavenger\Internals\ScavengerException;
 
 require_once(APP_ROOT . "/Helpers.php");
-
-try {
-	$d = new BaseDatabase();
-} catch (ScavengerException $e) {
-	die($e->getFancyMessage());
-}
